3188f41057bf5bba81c8235a4f09357aac0e7e54
William Summe <wlsumme@gmail.com>
2024-05-01 14:33:09 -0500

    DonZoo

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -8,15 +8,16 @@
 
             static void Main(string[] args)
             {
-                //var answer = 4;
+                var answer = 4;
 
-                //Console.WriteLine(answer < 9 ? $"{answer} is less than nine." : $"{answer} is greater than nine.");
+                Console.WriteLine(answer < 9 ? $"{answer} is less than nine." : $"{answer} is greater than nine.");
 
 
 
                 //teachers answer
-                var answer = 4;
-                var response = (answer < 9) ? $"{answer} is ess than nine." : $"{answer} is more than nine.";
+                //var answer = 4;
+                //var response = (answer < 9) ? $"{answer} is ess than nine." : $"{answer} is more than nine.";
+                //Console.WriteLine(response);
                     
 
 
